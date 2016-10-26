22cee2f7ef1a4a9f96baefdcdd150c0c3efcd4db
Jerome Hardaway <jeromedhardaway@gmail.com>
2016-10-26 09:47:27 -0500

    Added players

--- studio_game.rb
+++ studio_game.rb
@@ -1,5 +1,8 @@
-greeting = "Welcome!"
-3.times do
-  puts greeting.upcase
-end
-puts Time.now
+name1 = 'larry'
+name2 = 'curly'
+name3 = 'moe'
+health1 = 60
+
+puts "Players: \n\t#{name1}\n\t#{name2}\n\t#{name3}"
+
+puts "#{name1}'s health is #{health1 * 3}"
